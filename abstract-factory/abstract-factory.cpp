7d040d1643c747f4edba0711bc641df73df3310d
Kiel Cruey <kscruey@gmail.com>
2023-12-06 13:08:55 -0700

    prettied up code

--- abstract-factory/abstract-factory.cpp
+++ abstract-factory/abstract-factory.cpp
@@ -13,76 +13,34 @@ public:
 class Soldier : public AbstractPerson
 { 
 public:
-    Soldier()
-    {
-        std::cout << "Soldier Created" << std::endl;
-    }
-    
-    ~Soldier()
-    {
-        std::cout << "Soldier Destroyed" << std::endl;
-    }
+    Soldier() { std::cout << "Soldier Created" << std::endl; }
+    ~Soldier() { std::cout << "Soldier Destroyed" << std::endl; }
 
     // ============ concrete functions -- getting overridden ============
-    void Attack()
-    {
-        std::cout << "Soldier Attacked" << std::endl;
-    }
-
-    void CheckHealth()
-    {
-        std::cout << "Soldier Checked Health" << std::endl;
-    }
+    void Attack() { std::cout << "Soldier Attacked" << std::endl; }
+    void CheckHealth() { std::cout << "Soldier Checked Health" << std::endl; }
 };
 
 class Archer : public AbstractPerson
 {
 public:
-    Archer()  
-    {
-        std::cout << "Archer Created" << std::endl;
-    }
-
-    ~Archer () 
-    {
-        std::cout << "Archer Destroyed" << std::endl;
-    }
+    Archer() { std::cout << "Archer Created" << std::endl; }
+    ~Archer () { std::cout << "Archer Destroyed" << std::endl; }
 
     // ============ concrete functions -- getting overridden ============
-    void Attack()
-    {
-        std::cout << "Archer Attacked" << std::endl;
-    }
-
-    void CheckHealth()
-    {
-        std::cout << "Archer Checked Health" << std::endl;
-    }
+    void Attack() { std::cout << "Archer Attacked" << std::endl; }
+    void CheckHealth() { std::cout << "Archer Checked Health" << std::endl; }
 };
 
 class Calvary : public AbstractPerson
 {
 public:
-    Calvary(/* args */)
-    {
-        std::cout << "Calvary Created" << std::endl;
-    }
-
-    ~Calvary()
-    {
-        std::cout << "Calvary Destroyed" << std::endl;
-    }
+    Calvary() { std::cout << "Calvary Created" << std::endl; }
+    ~Calvary() { std::cout << "Calvary Destroyed" << std::endl; }
 
     // ============ concrete functions -- getting overridden ============
-    void Attack()
-    {
-        std::cout << "Calvary Attacked" << std::endl;
-    }
-
-    void CheckHealth()
-    {
-        std::cout << "Calvary Checked Health" << std::endl;
-    }
+    void Attack() { std::cout << "Calvary Attacked" << std::endl; }
+    void CheckHealth() { std::cout << "Calvary Checked Health" << std::endl; }
 };
 
 // ============ ABSTRACT FACTORY ============
@@ -101,15 +59,9 @@ public:
 class HumanFactory : public AbstractRaceFactory
 {
 public:
-    HumanFactory() 
-    {
-        std::cout << "Human Factory Created" << std::endl;
-    }
+    HumanFactory() { std::cout << "Human Factory Created" << std::endl; }
 
-    ~HumanFactory() 
-    {
-        std::cout << "Human Factory Destroyed" << std::endl;
-    }
+    ~HumanFactory() { std::cout << "Human Factory Destroyed" << std::endl; }
 
     AbstractPerson * MakeSoldier()
     {
@@ -130,15 +82,9 @@ public:
 class OrcFactory : public AbstractRaceFactory
 {
 public:
-    OrcFactory() 
-    {
-        std::cout << "Orc Factory Created" << std::endl;
-    }
+    OrcFactory() { std::cout << "Orc Factory Created" << std::endl; }
 
-    ~OrcFactory() 
-    {
-        std::cout << "Orc Factory Destroyed" << std::endl;
-    }
+    ~OrcFactory() { std::cout << "Orc Factory Destroyed" << std::endl; }
 
     AbstractPerson * MakeSoldier()
     {
