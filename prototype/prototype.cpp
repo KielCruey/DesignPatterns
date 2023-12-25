5e361de86a013fb3df251804fee7d0450e4c4bb4
Kiel Cruey <kiel.cruey@boydcorp.com>
2023-12-25 16:37:49 -0700

    wip -- added subclasses to prototype example

--- prototype/prototype.cpp
+++ prototype/prototype.cpp
@@ -1,7 +1,42 @@
+#include <string>
+
 // ============== Abstract Product ==============
-class prototype
+class AbstractRobot
 {
 public:
-    virtual ~prototype();
-    virtual prototype * Clone();
+    virtual ~AbstractRobot();
+    virtual AbstractRobot * Clone() = 0;
+};
+
+// ============== Concrete Product ==============
+class ConcreteWorkerRobot : public AbstractRobot
+{
+public:
+    ConcreteWorkerRobot()
+    {
+
+    }
+
+    ~ConcreteWorkerRobot()
+    {
+
+    }
+
+private:
+    std::string name;
+    int serialNumber;
+};
+
+class ConcreteArmyRobot : public AbstractRobot
+{
+public:
+    ConcreteArmyRobot()
+    {
+
+    }
+
+    ~ConcreteArmyRobot()
+    {
+
+    }
 };
