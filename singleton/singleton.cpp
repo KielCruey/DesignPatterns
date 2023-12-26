ae659bd2316917f7c597420d335220d8850a6403
Kiel Cruey <kiel.cruey@boydcorp.com>
2023-12-25 19:05:15 -0700

    added code to main function

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,33 +1,33 @@
 #include <string>
-
-
+#include <iostream>
 
 class Globals
 {
 private:
     // creating private constructor, since only static methods can access the constructor only
     Globals() : systemState(static_cast<int>(standardMode)), numberOfFiles(0) {}
+
     Globals(int systemState, int numberOfFiles)
     {
         this->systemState = systemState;
         this->numberOfFiles = numberOfFiles;
     }
 
+    // ========== member variables ==========
     static Globals * pGlobals;
-
-    // variables here:
     int systemState;
     int numberOfFiles;
     
 public:
     enum state { standardMode, calibrationMode, maintenanceMode, idleMode } state;
 
-
     static Globals * getInstance()
     {
         // checking if no instance of class
-        if (pGlobals == NULL) 
+        if (pGlobals == nullptr) 
         {
+            std::cout << "Creating a new Globals object" << std::endl;
+
             // We can access private members within the class.
             pGlobals = new Globals(); 
             
@@ -35,14 +35,18 @@ public:
         }
         else
         {
+            std::cout << "Globals object exists, returning existing Globals object" << std::endl;
+
             // if instancePtr != NULL that means the class already have an instance. 
             // So, we are returning that instance and not creating new one.
             return pGlobals;
         }
     }
 
-    // deleting copy constructor
+    // deleting copy constructor -- can be clone-able
     Globals(const Globals& obj) = delete; 
+    // deleting assignment operator -- can't use the "=" operator
+    void operator=(const Globals &) = delete;
 
     // ============ getters ============ 
     int GetSystemState()
@@ -72,7 +76,10 @@ Globals * Globals::pGlobals = nullptr;
 
 int main()
 {
+    // directly calling the getInstance() method which act as the constructor
+    Globals * pMainGlobals = Globals::getInstance();
 
-
+    pMainGlobals->SetNumberOfFiles(4);
+    int state = pMainGlobals->GetSystemState();
     return 0;
 }
