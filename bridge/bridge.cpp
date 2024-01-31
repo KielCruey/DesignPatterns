b79320e0b7a43d49e52e2497c8a81a5e1d4c2a8a
Kiel Cruey <kscruey@gmail.com>
2024-01-30 22:15:53 -0700

    layed down initial bridge pattern structure -- code compiles and works

--- bridge/bridge.cpp
+++ bridge/bridge.cpp
@@ -1,15 +1,222 @@
+#include <iostream>
+
 #include "bridge.hpp"
+
+enum State { OFF = 0, ON };
+
+int const MAXIMUM_VOLUME = 10;
+int const MINIMUM_VOLUME = 0;
+
+int const MAXIMUM_CHANNEL = 100;
+int const MINIMUM_CHANNEL = 1;
+
+// ============= TVDevice =============
+TVDevice::TVDevice(bool isMuted, bool isPowered, int volume, int channel, double watchTime, double totalTime) :
+    isMuted(isMuted),
+    isPowered(isPowered),
+    volume(volume),
+    channel(channel),
+    watchTime(watchTime),
+    totalTime(totalTime)
+{
+    std::cout << "TV Device created" << std::endl;
+}
+
+TVDevice::~TVDevice()
+{
+    std::cout << "TV Device destroyed" << std::endl;
+}
+
+void TVDevice::SetIsMuted(bool isMuted)
+{
+    this->isMuted = isMuted;
+}
+
+void TVDevice::SetPower(bool isPowered)
+{
+    this->isPowered = isPowered;
+}
+
+void TVDevice::SetVolume(int volume)
+{
+    this->volume = volume;
+}
+
+void TVDevice::SetChannel(int channel) 
+{
+    this->channel = channel;
+}
+
+void TVDevice::SetWatchTime(double watchTime)
+{
+    this->watchTime = watchTime;
+}
+
+void TVDevice::SetTotalTime(double totalTime)
+{
+    this->totalTime = totalTime;
+}
+
+bool TVDevice::GetIsMuted()
+{
+    return this->isMuted;
+}
+
+bool TVDevice::GetPower() 
+{
+    return this->isPowered;
+}
+
+int TVDevice::GetVolume() 
+{
+    return this->volume;
+}
+
+int TVDevice::GetChannel() 
+{
+    return this->channel;
+}
+
+double TVDevice::GetWatchTime()
+{
+    return this->watchTime;
+}
+
+double TVDevice::GetTotalTime()
+{
+    return this->totalTime;
+}
+
+// ============= RadioDevice =============
+RadioDevice::RadioDevice(bool isMuted, bool isPowered, int volume, int channel) :
+    isPowered(isPowered),
+    volume(volume),
+    channel(channel)
+{
+    std::cout << "Radio Device created" << std::endl;
+}
+
+RadioDevice::~RadioDevice()
+{
+    std::cout << "Radio Device destroyed" << std::endl;
+}
+
+void RadioDevice::SetIsMuted(bool isMuted)
+{
+    this->isMuted = isMuted;
+}
+
+void RadioDevice::SetPower(bool isPowered)
+{
+    this->isPowered = isPowered;
+}
+
+void RadioDevice::SetVolume(int volume)
+{
+    this->volume = volume;
+}
+
+void RadioDevice::SetChannel(int channel)
+{
+    this->channel  = channel;
+}
+
+bool RadioDevice::GetIsMuted()
+{
+    return this->isMuted;
+}
+
+bool RadioDevice::GetPower()
+{
+    return this->isPowered;
+}
+
+int RadioDevice::GetVolume()
+{
+    return this->volume;
+}
+
+int RadioDevice::GetChannel()
+{
+    return this->channel;
+}
 
 // ============= Abstraction =============
 Remote::Remote(Device * device) :
     device(device)
 {
-
+    std::cout << "Remote Device created" << std::endl;
 }
 
 Remote::~Remote()
 {
+    std::cout << "Remote Device destroyed" << std::endl;
+}
 
+// turns the power on/off
+bool Remote::TogglePower(Device * device)
+{
+    if(device->GetPower())
+        device->SetPower(State::OFF);
+    else
+        device->SetPower(State::ON);
+
+    return device->GetPower();
+}
+
+// turns the volume on/off
+bool Remote::ToggleMute(Device * device)
+{
+    if(device->GetIsMuted())
+        device->SetIsMuted(State::OFF);
+    else
+        device->SetIsMuted(State::ON);
+
+    return device->GetIsMuted();
+}
+
+// increases volume by one and check if not over maximum volume
+int Remote::VolumeUp(Device * device)
+{
+    if(device->GetVolume() < MAXIMUM_VOLUME)
+    {
+        device->SetVolume(device->GetVolume() + 1);
+    }
+
+    return device->GetVolume();
+}   
+
+// decreases volume by one and check if not over minimum volume
+int Remote::VolumeDown(Device * device)
+{
+    if(device->GetVolume() > MINIMUM_VOLUME)
+    {
+        device->SetVolume(device->GetVolume() - 1);
+    }
+
+    return device->GetVolume();
+}
+
+// increases channel by one and check if not over maximum channel
+int Remote::ChannelUp(Device * device)
+{
+    if(device->GetChannel() < MAXIMUM_CHANNEL)
+    {
+        device->SetChannel(device->GetChannel() + 1);
+    }
+
+    return device->GetChannel();
+}
+
+// decrease channel by one and check if not over minimum channel
+int Remote::ChannelDown(Device * device)
+{
+    if(device->GetChannel() > MINIMUM_CHANNEL)
+    {
+        device->SetChannel(device->GetChannel() - 1);
+    }
+
+    return device->GetChannel();
 }
 
 // ============= Redefined Abstraction =============
@@ -17,16 +224,108 @@ Remote::~Remote()
 TVRemote::TVRemote(Device * device) :
     Remote(device)
 {
-    
+    std::cout << "TV Remote Device created" << std::endl;
 }
 
 TVRemote::~TVRemote()
 {
-    
+    std::cout << "TV Remote Device destroyed" << std::endl;
+}
+
+bool TVRemote::TogglePower(Device * device)
+{
+    if(device->GetPower())
+        device->SetPower(State::OFF);
+    else
+        device->SetPower(State::ON);
+
+    return device->GetPower();
+}
+
+bool TVRemote::ToggleMute(Device * device)
+{
+    if(device->GetIsMuted())
+        device->SetIsMuted(State::OFF);
+    else
+        device->SetIsMuted(State::ON);
+
+    return device->GetIsMuted();
+}
+
+int TVRemote::VolumeUp(Device * device)
+{
+    if(device->GetVolume() < MAXIMUM_VOLUME)
+    {
+        device->SetVolume(device->GetVolume() + 1);
+    }
+
+    return device->GetVolume();
+}
+
+int TVRemote::VolumeDown(Device * device)
+{
+    if(device->GetVolume() > MINIMUM_VOLUME)
+    {
+        device->SetVolume(device->GetVolume() - 1);
+    }
+
+    return device->GetVolume();
+}
+
+int TVRemote::ChannelUp(Device * device)
+{
+    if(device->GetChannel() < MAXIMUM_CHANNEL)
+    {
+        device->SetChannel(device->GetChannel() + 1);
+    }
+
+    return device->GetChannel();
+}
+
+int TVRemote::ChannelDown(Device * device)
+{
+    if(device->GetChannel() > MINIMUM_CHANNEL)
+    {
+        device->SetChannel(device->GetChannel() - 1);
+    }
+
+    return device->GetChannel();
+}
+
+int TVRemote::FastRewind(Device * device)
+{
+    return 0;
+}
+
+int TVRemote::Rewind(Device * device)
+{   
+    return 0;
+}
+
+int TVRemote::Play(Device * device)
+{
+    return 0;
+}
+
+int TVRemote::FastForward(Device * device)
+{
+    return 0;
 }
 
 // =============== Main ===============
 int main()
 {
+    Device * radioDevice = new RadioDevice();
+    Remote * radioRemote = new Remote(radioDevice);
+
+    delete radioDevice;
+    delete radioRemote;
+
+    TVDevice * tvDevice = new TVDevice();
+    TVRemote * tvRemote = new TVRemote(tvDevice);
+
+    delete tvDevice;
+    delete tvRemote;
+
     return 0;
 }
--- bridge/bridge.hpp
+++ bridge/bridge.hpp
@@ -1,17 +1,99 @@
+// ============= Implementation =============
+class Device
+{
+public:
+    virtual ~Device() = default;
+
+    virtual void SetIsMuted(bool isMuted) = 0;
+    virtual void SetPower(bool power) = 0;
+    virtual void SetVolume(int volume) = 0;
+    virtual void SetChannel(int channel) = 0;
+
+    virtual bool GetIsMuted() = 0;
+    virtual bool GetPower() = 0;
+    virtual int GetVolume() = 0;
+    virtual int GetChannel() = 0;
+};
+
+// ============= Concrete Implementation =============
+class TVDevice : public Device
+{
+public:
+    TVDevice(bool isMuted = 0, 
+            bool isPowered = 0, 
+            int volume = 0, 
+            int channel = 1, 
+            double watchTime = 0, 
+            double totalTime = 0);
+            
+    virtual ~TVDevice() override;
+
+    virtual void SetIsMuted(bool isMuted) override;
+    virtual void SetPower(bool isPowered) override;
+    virtual void SetVolume(int volume) override;
+    virtual void SetChannel(int channel) override;
+
+    void SetWatchTime(double watchTime);
+    void SetTotalTime(double totalTime);
+
+    virtual bool GetIsMuted() override;
+    virtual bool GetPower() override;
+    virtual int GetVolume() override;
+    virtual int GetChannel() override;
+
+    double GetWatchTime();
+    double GetTotalTime();
+
+private:
+    bool isMuted;
+    bool isPowered;
+    int volume;
+    int channel;
+    double watchTime; // tv feature only
+    double totalTime; // tv feature only
+};
+
+class RadioDevice : public Device
+{
+public:
+    RadioDevice(bool isMuted = 0,
+                bool isPowered = 0,
+                int volume = 0,
+                 int channel = 1);
+                 
+    virtual ~RadioDevice() override;
+
+    virtual void SetIsMuted(bool isMuted) override;
+    virtual void SetPower(bool isPowered) override;
+    virtual void SetVolume(int volume) override;
+    virtual void SetChannel(int channel) override;
+
+    virtual bool GetIsMuted() override;
+    virtual bool GetPower() override;
+    virtual int GetVolume() override;
+    virtual int GetChannel() override;
+
+private:
+    bool isMuted;
+    bool isPowered;
+    int volume;
+    int channel;
+};
+
 // ============= Abstraction =============
 // only put in functions that the abstract class needs to do,
 class Remote
 {
 public:
     Remote(Device * device);
-    virtual ~Remote() = default;
+    virtual ~Remote();
 
-    virtual bool TogglePower() = 0;
-    virtual bool ToggleMute() = 0;
-    virtual int VolumeUp() = 0;
-    virtual int VolumeDown() = 0;
-    virtual int ChannelUp() = 0;
-    virtual int ChannelDown() = 0;
+    virtual bool TogglePower(Device * device);
+    virtual bool ToggleMute(Device * device);
+    virtual int VolumeUp(Device * device);
+    virtual int VolumeDown(Device * device);
+    virtual int ChannelUp(Device * device);
+    virtual int ChannelDown(Device * device);
 
 // device needs to be accessible to the derived classes -- used protected
 protected:
@@ -23,68 +105,18 @@ class TVRemote : public Remote
 {
 public:
     TVRemote(Device * device);
-    ~TVRemote() override;
+    virtual ~TVRemote();
 
-    bool TogglePower() override;
-    bool ToggleMute() override;
-    int VolumeUp() override;
-    int VolumeDown() override;
-    int ChannelUp() override;
-    int ChannelDown() override;
+    virtual bool TogglePower(Device * device) override;
+    virtual bool ToggleMute(Device * device) override;
+    virtual int VolumeUp(Device * device) override;
+    virtual int VolumeDown(Device * device) override;
+    virtual int ChannelUp(Device * device) override;
+    virtual int ChannelDown(Device * device) override;
 
     // tv only features
-    int FastRewind(); // rewinds only 10 seconds
-    int Rewind();
-    int Play();
-    int FastForward();
+    int FastRewind(Device * device); // rewinds only 10 seconds
+    int Rewind(Device * device);
+    int Play(Device * device);
+    int FastForward(Device * device);
 };
-
-// ============= Implementation =============
-class Device
-{
-public:
-    virtual ~Device() = default;
-
-    virtual void SetPower(bool power) = 0;
-    virtual void SetVolume(int volume) = 0;
-    virtual void SetChannel(int channel) = 0;
-
-    virtual bool GetPower() = 0;
-    virtual int GetVolume() = 0;
-    virtual int GetChannel() = 0;
-};
-
-// ============= Concrete Implementation =============
-class TVDevice : public Device
-{
-public:
-    TVDevice();
-    ~TVDevice() override;
-
-    void SetPower(bool power) override;
-    void SetVolume(int volume) override;
-    void SetChannel(int channel) override;
-
-    bool GetPower() override;
-    int GetVolume() override;
-    int GetChannel() override;
-
-private:
-    bool power;
-    int volume;
-    int channel;
-    double time; // tv feature only
-};
-
-class RadioDevice : public Device
-{
-public:
-    RadioDevice();
-    ~RadioDevice();
-
-private:
-    bool power;
-    int volume;
-    int channel;
-};
-
