1a7ef07e041d774746a00ce8ea3628f1d420f7a5
Kiel Cruey <kiel.cruey@boydcorp.com>
2024-03-01 13:36:06 -0700

    updated header class blueprint

--- iterator/iterator.hpp
+++ iterator/iterator.hpp
@@ -1,17 +1,30 @@
-
-
-
+// =========== Abstract Iterator ===========
 class AbstractIterator
 {
 public:
 	virtual ~AbstractIterator();
+
+	virtual void first() = 0;
+	virtual void next() = 0;
+	virtual void hasNext() = 0;
+	virtual void current() = 0;
 };
 
+class LinkedListIterator
+{
+public:
+	virtual ~LinkedListIterator();
+};
 
+// =========== Abstract Collection ===========
 class AbstractCollection
 {
 public:
 	virtual ~AbstractCollection();
+
+	virtual void addItem() = 0;
+	virtual void removeItem() = 0;
+	virtual bool isEmpty() = 0;
 };
 
 class LinkedList : AbstractCollection
